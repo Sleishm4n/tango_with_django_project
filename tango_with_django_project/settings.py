8616de9b5023cf18f3d9649d1e4c091774fa73ce
Sam Leishman <2888936l@student.gla.ac.uk>
2025-02-02 22:43:06 +0000

    Chapter 11

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
+    'registration',
 ]
 
 MIDDLEWARE = [
@@ -134,3 +135,13 @@ MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL ='/media/'
 
 LOGIN_URL = 'rango:login'
+
+# If True, users can register.
+REGISTRATION_OPEN = True
+# If True, the user will be automatically logged in after registering.
+REGISTRATION_AUTO_LOGIN = True
+# The URL that Django redirects users to after logging in.
+LOGIN_REDIRECT_URL = 'rango:index'
+# The page users are directed to if they are not logged in.
+# This was set in a previous chapter. The registration package uses this, too.
+LOGIN_URL = 'auth_login'
